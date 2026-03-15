a69fe3cd5a896f82f3a6827459d535d182527fb8
Reo <pokk123123a@gmail.com>
2026-03-15 22:20:59 +0900

    2장1 정리내용

--- Hello World.py
+++ Hello World.py
@@ -1,1 +1,22 @@
-printf("Hello, World!")
+print("Hello, World!")
+
+name = "Reo"
+print(f"My name is {name}")
+
+introduce = f"hi, my name is {name}"
+
+age = 20
+
+print(f"i am {age:05d} years old")
+print(f"i am {age:-<5d} years old")
+print(f"i am {age:*^5d} years old\n")
+
+name1 = "철수"
+score1 = 95
+# C 스타일: %s(문자열), %d(정수)
+print("이름: %s, 점수: %d점" % (name1, score1))
+
+pi = 3.141592653589793
+print(f"{pi:.2f}")
+
+print(f"pi : {pi:$<10.2f}")
