4cd243f066685f8549960f69e035913bb75d5b65
Reo <pokk123123a@gmail.com>
2026-03-16 15:32:18 +0900

    260316 파이썬실습문제

---
+++ Python Class Work/2_test.py
@@ -0,0 +1,45 @@
+# x = 100
+# print("x = ", x)
+# y = 200
+# print("y = ", y)
+
+# sum = x + y
+# print("sum = ", sum)
+
+# name = "Reo"
+# address = "서울시 노원구"
+# print(name,"\n",address)
+# print(name + "\n" + address)
+# print("{0}\n{1}".format(name, address))
+# print(f"{name}\n{address}")
+
+# score = 10
+# score = score + 1
+# print(score)
+# score += 1
+# print(score)
+
+# x = 100
+# y = 200
+# sum = x + y
+# print(x,"와", y, "의 합은", sum, "입니다.")
+# print(f"{x}와 {y}의 합은 {sum}입니다.")
+
+# x = int(input("첫 번째 숫자를 입력하시오: "))
+# y = int(input("두 번째 숫자를 입력하시오: "))
+# sum = x + y
+# print(x,"와", y, "의 합은", sum, "입니다.")
+# print(f"{x}와 {y}의 합은 {sum}입니다.")
+
+# name = input("이름을 입력하시오: ")
+# print(f"안녕하세요, {name}님!\n파이썬에 오신 것을 환영합니다.\n")
+
+
+name = input("이름을 입력하시오: ")
+print(f"안녕하세요, {name}씨!\n파이썬에 오신 것을 환영합니다.\n")
+x = int(input("첫 번째 정수를 입력하시오:  "))
+y = int(input("두 번째 정수를 입력하시오:  "))
+print("=" * 30)
+print(f"{x}와 {y}의 합은 {x+y}입니다.\n")
+print(f"{x}와 {y}의 차는 {x-y}입니다.\n")
+print(f"{x}와 {y}의 곱은 {x*y}입니다.\n")
