cb7c4d073a4b3b421343fb41377efe1c22abd0f5
Reo <pokk123123a@gmail.com>
2026-03-22 19:52:52 +0900

    match-case 정리

--- ch.3/if_practice.py
+++ ch.3/if_practice.py
@@ -13,3 +13,25 @@ print(result)
 score = 85
 result = "합격" if score >= 60 else "불합격"
 print(result)
+
+
+#match-case 문
+grade = 'B'
+match grade:
+    case 'A':
+        print("탁월한 성적입니다.")
+    case 'B':
+        print("우수한 성적입니다.")
+    case 'C':
+        print("보통입니다.")
+    case _:
+        print("노력이 필요합니다.")
+
+
+#여러패턴을 하나에 case로 묶을 수 있다.
+grade = "B"
+match grade:
+    case "A" | "B" | "C":
+        print("합격입니다.")
+    case _:
+        print("불합격입니다.")
