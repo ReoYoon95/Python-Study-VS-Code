531dee4ec214e5a600aa2b4cecceacf3dac3658d
Reo <pokk123123a@gmail.com>
2026-03-22 19:51:23 +0900

    if문의 조건부 표현식(한줄)

---
+++ ch.3/if_practice.py
@@ -0,0 +1,15 @@
+# 조건부 표현식
+
+score = 85
+if score >= 60:
+    result = "합격"
+else:
+    result = "불합격"
+print(result)
+
+
+# 이런 간단한 조건 분기는 파이썬의 조건부 표현식을 사용하면 한 줄로 표현할 수 있다.
+
+score = 85
+result = "합격" if score >= 60 else "불합격"
+print(result)
