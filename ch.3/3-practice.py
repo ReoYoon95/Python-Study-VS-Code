82093088a64f36011546f108c7e8577853e09405
Reo <pokk123123a@gmail.com>
2026-03-24 23:16:07 +0900

    ch3 실습문제2

--- ch.3/3-practice.py
+++ ch.3/3-practice.py
@@ -14,3 +14,26 @@ else:
     print("none")
 # #여기서 참인 것은 shirt 와 need, 그러나 shirt가 먼저이기에 shirt만 출력됌.
 
+# 실습문제2
+# whiel문 이용 1~1000까지의 자연수중 3의배수의 합
+
+i = 1
+result = 0
+while i <= 1000:
+    if i % 3 == 0:
+        result += i
+        i += 1
+    else:
+        i += 1
+print(result)
+
+# for문으로 한다면??
+
+result = 0
+for i in range(3, 1001, 3):
+    result += i
+print(result)
+
+# sum()사용 한다면??
+
+print(sum(range(3, 1001, 3)))
