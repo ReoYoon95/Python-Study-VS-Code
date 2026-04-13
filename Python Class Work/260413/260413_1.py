6a35ff5033ebb1e7b822d223a602ccbc9e0acee5
Reo <pokk123123a@gmail.com>
2026-04-13 14:56:08 +0900

    260413 1번커밋

---
+++ Python Class Work/260413/260413_1.py
@@ -0,0 +1,17 @@
+print("방문을 환영합니다")
+print("방문을 환영합니다")
+print("방문을 환영합니다")
+print("방문을 환영합니다")
+print("방문을 환영합니다")
+
+
+for i in range(5) :
+    print("방문을 환영합니다!")
+
+
+
+# A:int = int(input("원하는 단을 입력하시오: "))
+
+# for i in range(1,10) :
+#     print(f"{A} * {i} = {A * i}")
+    
